c0cb6bf5cb400a280e737cfe2e2f849ee89898e9
zafex <fajrulaz@gmail.com>
2019-02-05 20:54:09 +0700

    how to use middleware

--- bootstrap.php
+++ bootstrap.php
@@ -5,66 +5,78 @@ defined('VILOVEUL_WORKDIR') or define('VILOVEUL_WORKDIR', __DIR__);
 // require composer autoloader
 require_once __DIR__ . '/vendor/autoload.php';
 
+// initialize container object
+$container = Viloveul\Container\ContainerFactory::instance();
+
+// setup configuration
+$configuration = new Viloveul\Config\Configuration([
+    /* your app name */
+    'name' => 'Viloveul',
+    /* app version */
+    'version' => '1.0',
+    /* root application */
+    'root' => __DIR__,
+    /* base pathinfo on url */
+    'basepath' => '/api/v1',
+    /* uploader */
+    'upload' => [
+        /* target upload */
+        'target' => __DIR__ . '/public/uploads',
+    ],
+    'auth' => [
+        /* token name on header for jwt */
+        'name' => 'Bearer',
+        /* private key for generate jwt */
+        'private' => __DIR__ . '/config/private.pem',
+        /* public key for read jwt */
+        'public' => __DIR__ . '/config/public.pem',
+        /* phrase */
+        'phrase' => 'something',
+    ],
+    /* db config (@see eloquent orm) */
+    'db' => [
+        'default' => [
+            // database driver
+            'driver' => 'mysql',
+            // database host
+            'host' => 'localhost',
+            // database port
+            'port' => 3306,
+            'database' => 'viloveul',
+            'username' => 'dev',
+            'password' => 'something',
+            'prefix' => 'tbl_',
+            'charset' => 'utf8',
+            'collation' => 'utf8_general_ci',
+        ],
+    ],
+    'cache' => [
+        /* ADAPTER (APCU|REDIS) */
+        'adapter' => 'apcu',
+        /* LIFETIME CACHE BEFORE DELETE */
+        'lifetime' => 3600,
+        /* CACHE PREFIX */
+        'prefix' => 'viloveul_',
+        /* CACHE HOST (REDIS) */
+        'host' => '127.0.0.1',
+        /* CACHE PORT (REDIS) */
+        'port' => 6379,
+    ],
+    'commands' => [
+        App\Command\HelloCommand::class,
+    ],
+]);
+
 // initialize application object with existing $container and $configs
-$app = new Viloveul\Kernel\Application(
-    new Viloveul\Container\Container(),
-    new Viloveul\Config\Configuration([
-        /* your app name */
-        'name' => 'Viloveul',
-        /* app version */
-        'version' => '1.0',
-        /* root application */
-        'root' => __DIR__,
-        /* base pathinfo on url */
-        'basepath' => '/api/v1',
-        /* uploader */
-        'upload' => [
-            /* target upload */
-            'target' => __DIR__ . '/public/uploads',
-        ],
-        'auth' => [
-            /* token name on header for jwt */
-            'name' => 'Bearer',
-            /* private key for generate jwt */
-            'private' => __DIR__ . '/config/private.pem',
-            /* public key for read jwt */
-            'public' => __DIR__ . '/config/public.pem',
-            /* phrase */
-            'phrase' => 'something',
-        ],
-        /* db config (@see eloquent orm) */
-        'db' => [
-            'default' => [
-                // database driver
-                'driver' => 'mysql',
-                // database host
-                'host' => 'localhost',
-                // database port
-                'port' => 3306,
-                'database' => 'viloveul',
-                'username' => 'dev',
-                'password' => 'something',
-                'prefix' => 'tbl_',
-                'charset' => 'utf8',
-                'collation' => 'utf8_general_ci',
-            ],
-        ],
-        'cache' => [
-            /* ADAPTER (APCU|REDIS) */
-            'adapter' => 'apcu',
-            /* LIFETIME CACHE BEFORE DELETE */
-            'lifetime' => 3600,
-            /* CACHE PREFIX */
-            'prefix' => 'viloveul_',
-            /* CACHE HOST (REDIS) */
-            'host' => '127.0.0.1',
-            /* CACHE PORT (REDIS) */
-            'port' => 6379,
-        ],
-        'commands' => [
-            App\Command\HelloCommand::class,
-        ],
-    ])
+$app = new Viloveul\Kernel\Application($container, $configuration);
+
+/**
+ * Register middleware
+ */
+$app->middleware(
+    function ($request, $next) {
+        return $next->handle($request);
+    }
 );
 
 /**
@@ -77,9 +89,9 @@ $app->uses(function (Viloveul\Router\Contracts\Collection $router) {
 });
 
 /**
- * Load all middlewares
+ * Load event listener
  */
-$app->uses(function (Viloveul\Kernel\Contracts\Middleware $middleware) {
+$app->uses(function (Viloveul\Event\Contracts\Dispatcher $event) {
     foreach (glob(__DIR__ . '/hooks/*.php') as $file) {
         require $file;
     }
